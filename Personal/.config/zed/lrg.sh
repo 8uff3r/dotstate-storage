82c5e4ea95b8b70f4a417c20a752416ab974bcf9
8uff3r <8uff3r@gmail.com>
2026-02-01 05:45:44 +0330

    Add 1 file, Update 1 file: lrg.sh, tasks.json

---
+++ Personal/.config/zed/lrg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+lg() {
+  local RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case"
+  local INITIAL_QUERY="${1:-}"
+
+  FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'" \
+    fzf --ansi --disabled --query "$INITIAL_QUERY" \
+        --bind "change:reload:$RG_PREFIX {q} || true" \
+        --delimiter ':' \
+        --preview 'bat --color=always --style=numbers --highlight-line {2}:{2} {1}' \
+        --preview-window '+{2}-5' \
+        --bind "enter:execute(${EDITOR:-vim} {1} +{2})"
+}
+
+lg "$@"
