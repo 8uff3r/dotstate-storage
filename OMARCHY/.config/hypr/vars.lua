b3e48d60388afbe1765d8b17cab250912e96c129
dotstate <dotstate@localhost>
2026-08-13 19:48:08 +0330

    Add 2 files, Update 2 files: env.lua, hyprland.lua, vars.lua, config.json

--- OMARCHY/.config/hypr/vars.lua
+++ OMARCHY/.config/hypr/vars.lua
@@ -1,20 +1,19 @@
-
 local M = {
-	---------------------
-	---- MY PROGRAMS ----
-	---------------------
-	terminal = "foot",
-	fileManager = "nautilus",
-	menu = "fuzzel",
-	editor = "zeditor",
-	---------------------
-	------ CURSORS ------
-	---------------------
-	cursorTheme = "Bibata-Modern-Classic",
-	cursorSize = "24",
+    ---------------------
+    ---- MY PROGRAMS ----
+    ---------------------
+    terminal = "foot",
+    fileManager = "dolphin",
+    menu = "fuzzel",
+    editor = "zeditor",
+    ---------------------
+    ------ CURSORS ------
+    ---------------------
+    cursorTheme = "Bibata-Modern-Classic",
+    cursorSize = "24",
 
-	touchpadDisableTyping = true,
-	touchpadScrollFactor = 0.3,
+    touchpadDisableTyping = true,
+    touchpadScrollFactor = 0.3,
 }
 
 return M
