07a8b333e93f288ff71753dbfe58cb5a7ffaa595
8uff3r <8uff3r@gmail.com>
2026-08-26 21:55:53 +0330

    Update 1 file: vars.lua

--- OMARCHY/.config/hypr/vars.lua
+++ OMARCHY/.config/hypr/vars.lua
@@ -3,7 +3,7 @@ local M = {
     ---- MY PROGRAMS ----
     ---------------------
     terminal = "foot",
-    fileManager = "dolphin",
+    fileManager = "hyprfm",
     menu = "fuzzel",
     editor = "zeditor",
     ---------------------
